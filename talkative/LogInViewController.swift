d434858d6a55b2e5677bdf5a3f3e63e5ff1f5b39
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-21 19:15:18 -0400

    add LogIn VC

---
+++ talkative/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  talkative
+//
+//  Created by Ziad Hamdieh on 2018-05-21.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func logInPressed(_ sender: AnyObject) {
+
+        
+    }
+    
+    
+}
