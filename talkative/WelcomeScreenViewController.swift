1d5908cf9b5e06cd58de1092e2546d6ca0951fed
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-21 19:00:34 -0400

    add VC for welcome screen

---
+++ talkative/WelcomeScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeScreenViewController.swift
+//  talkative
+//
+//  Created by Ziad Hamdieh on 2018-05-21.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeScreenViewController : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
