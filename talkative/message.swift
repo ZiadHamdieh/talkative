34d7ef2610ec8e35769a180b8a242064bd2d2831
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-21 18:58:04 -0400

    setup MVC skeleton

---
+++ talkative/message.swift
@@ -0,0 +1,13 @@
+//
+//  message.swift
+//  talkative
+//
+//  Created by Ziad Hamdieh on 2018-05-21.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+}
