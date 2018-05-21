d2755d07554a1e872254066cfdd98fdca4435d9b
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-21 19:19:56 -0400

    add Chat VC

---
+++ talkative/ChatViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ChatViewController.swift
+//  talkative
+//
+//  Created by Ziad Hamdieh on 2018-05-21.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+
+class ChatViewController: UIViewController {
+    
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var messageTextfield: UITextField!
+    @IBOutlet var messageTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+    
+    
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        
+    
+        
+        
+    }
+
+    
+    
+    @IBAction func logOutPressed(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+    
+    
+}
