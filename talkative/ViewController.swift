477f7432063b5f6b3c107aceb4983fc43fbd04ce
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-21 17:37:29 -0400

    initial commit

---
+++ talkative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  talkative
+//
+//  Created by Ziad Hamdieh on 2018-05-21.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
