a8fad99a1371d642e99b0c7e65f058e3fd11f894
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-28 04:39:45 -0400

    add firebase configuration in appDelegate

---
+++ Pods/Target Support Files/Pods-talkative/Pods-talkative-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_talkative : NSObject
+@end
+@implementation PodsDummy_Pods_talkative
+@end
--- talkative/AppDelegate.swift
+++ talkative/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // TODO: initialize and configure firebase
+        FirebaseApp.configure()
+        
+        let db = Database.database().reference()
+        db.setValue("firebase test")
         
         return true
     }
