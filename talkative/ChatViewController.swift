af363600a70a775e7b25a43930852f383af45f48
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-29 06:07:23 -0400

    add background color to chat screen

--- talkative/ChatViewController.swift
+++ talkative/ChatViewController.swift
@@ -142,6 +142,7 @@ extension ChatViewController: UITextFieldDelegate, UITableViewDelegate, UITableV
         let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageCell
         cell.message.text = messages[indexPath.row].messageContent
         cell.userName.text = messages[indexPath.row].sender
+        cell.backgroundColor = UIColor(rgb: 0xd6f5fc)
         
         let image = UIImage(named: "defaultAvatar")
         cell.userImageView.image = image
@@ -186,5 +187,24 @@ extension ChatViewController: UITextFieldDelegate, UITableViewDelegate, UITableV
             self.view.layoutIfNeeded()
         }
         
+    }
+}
+
+// Extra extension to UIColor to use hexadecimal values in setting background colors
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
     }
 }
